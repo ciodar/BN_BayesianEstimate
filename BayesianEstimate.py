Corrected BE algorithm and KL divergency. Implemented the save of pseudocounts for successive estimates.

BayesianEstimate.py
@@ -1,9 +1,10 @@
 import csv
 import numpy as np
+import copy
 from BayesianNetwork import BayesianNet
 from KLDivergenceCalculation import combinations
 
-def bayes_estimator(bn, csvFile):
+def bayes_estimator(csvFile,bn):
     """
     Bayesian Estimation method of parameter learning.
     This method proceeds by either 1) assuming a uniform prior
@@ -60,29 +61,38 @@ def bayes_estimator(bn, csvFile):
             data.append(row)
     equiv_sample = len(data)
 
-    obs_dict = dict([(rv, []) for rv in bn.getNodes()])
+    obs_dict = dict([(rv, []) for rv in bn.getNodeKeys()])
     # set empty conditional probability table for each RV
     for rv in bn.getNodes():
         # get number of values in the CPT = product of scope vars' cardinalities
-        p_idx = int(np.prod([bn.nodes[p].card() for p in bn.nodes[rv].parents]) * bn.nodes[rv].card())
-        bn.nodes[rv].cpt = [equiv_sample / p_idx] * p_idx
+        p_idx = int(np.prod([bn.nodes[p].card() for p in rv.parents]) * rv.card())
+        if rv.alphas != list(np.zeros(shape=(p_idx,1))):
+            rv.cpt = copy.deepcopy(rv.alphas)
+        else:
+        #Sets uniform distribution for each cell of cpt
+            rv.cpt = [equiv_sample / p_idx] * p_idx
 
     # loop through each row of data
     for row in data:
         # store the observation of each variable in the row
-        obs_dict = dict([(rv, row[rv]) for rv in bn.getNodes()])
+        obs_dict = dict([(rv, row[rv]) for rv in bn.getNodeKeys()])
         # loop through each RV and increment its observed parent-self value
         for rv in bn.getNodes():
-            rv_dict = {n: obs_dict[n] for n in obs_dict if n in bn.nodes[rv].scope()}
-            offset = bn.cpt_indices(target=bn.nodes[rv], val_dict=rv_dict)
-            bn.nodes[rv].cpt[offset] += 1
+            rv_dict = {n: obs_dict[n] for n in obs_dict if n in rv.scope()}
+            offset = bn.cpt_indices(target=rv, val_dict=rv_dict)
+            rv.cpt[offset] += 1
+            rv.alphas[offset] +=1
 
     for rv in bn.getNodes():
-        cpt = bn.nodes[rv].cpt
-        for i in range(0, len(bn.nodes[rv].cpt), bn.nodes[rv].card()):
-            temp_sum = float(np.sum(cpt[i:(i + bn.nodes[rv].card())]))
-            for j in range(bn.nodes[rv].card()):
+        cpt = rv.cpt
+        alphas = rv.alphas
+        for i in range(0, len(rv.cpt), rv.card()):
+            temp_sum = float(np.sum(cpt[i:(i + rv.card())]))
+            #alphas_sum = float(np.sum(alphas[i:(i+rv.card())]))
+            for j in range(rv.card()):
+                #
                 cpt[i + j] /= (temp_sum)
+                #Rounds the calculated posterior probability to 2nd decimal
                 cpt[i + j] = round(cpt[i + j], 5)
                 #if(bn.nodes[rv].parents):
                     #print('P(',bn.nodes[rv].name,'=')
@@ -102,5 +112,5 @@ if __name__ == "__main__":
         reader = csv.DictReader(csvfile, delimiter=';')
         tuples = []
         for row in reader:
-            tuples.append(tuple(row[v] for v in bn.getNodes()))
+            tuples.append(tuple(row[v] for v in bn.getNodeKeys()))
         print(tuples)

BayesianNetwork.py
@@ -3,7 +3,7 @@ from KLDivergenceCalculation import combinations
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-import itertools
+#import itertools
 import json
 import csv
 
@@ -29,45 +29,60 @@ class BayesianNet:
             self.nodes[v].domain=inputNetwork['F'][v]['values']
             for p in inputNetwork['F'][v]['parents']:
                 self.nodes[v].parents.append(p)
-            self.nodes[v].cpt=np.ones(shape=(len(inputNetwork['F'][v]['cpt']),1))
-            self.nodes[v].obs = np.zeros(shape=(len(inputNetwork['F'][v]['cpt']), 1))
+            self.nodes[v].cpt=np.zeros(shape=(len(inputNetwork['F'][v]['cpt']),1))
+            self.nodes[v].alphas = inputNetwork['F'][v]['alphas']
             self.nodes[v].originalcpt = inputNetwork['F'][v]['cpt']
 
     def getDivergenceKL(self):
         return self.divergenceKL
     def setDivergenceKL(self, div):
         self.divergenceKL = div
+    def getNode(self,key):
+        return self.nodes[key]
     def getNodes(self):
+        return list(self.nodes.values())
+    def getNodeKeys(self):
         return list(self.nodes.keys())
 
     def factor(self,rv,val_dict):
-        return rv.cpt[self.cpt_idx(rv,val_dict)]
+        return rv.cpt[self.cpt_indices(rv,val_dict)]
     def original_factor(self,rv,val_dict):
         return rv.originalcpt[self.original_cpt_indices(rv,val_dict)]
 
     def combination_size(self):
         size = 1
         for v in self.getNodes():
-            size *= self.nodes[v].card()
+            size *= v.card()
         return size
+    def stride(self, rv, n):
+        if n==rv.name:
+            return 1
+        else:
+            card_list = [rv.card()]
+            card_list.extend([self.nodes[p].card() for p in rv.parents])
+            n_idx = rv.parents.index(n) + 1
+            return int(np.prod(card_list[0:n_idx]))
 
     def full_joint(self,val_dict):
         prod = 1
-        for i in self.nodes.values():
-            prod*= self.factor(i,val_dict)
+        for n in self.getNodes():
+            t = dict((k, val_dict[k]) for k in n.scope())
+            prod*= self.factor(n,t)
         return prod
 
     def original_full_joint(self,val_dict):
         prod = 1
-        for i in self.nodes.values():
-            prod*= self.original_factor(i,val_dict)
+        for n in self.getNodes():
+            t = dict((k, val_dict[k]) for k in n.scope())
+            prod*= self.original_factor(n,t)
         return prod
 
     def cpt_idx(self, target, tuple):
         d = []
-        comb = combinations(v for v in target.scope())
+        comb = combinations(self.nodes[v] for v in target.scope())
         idx = [i for i, n in enumerate(comb) if comb[i] == tuple][0]
         return idx
+
     def cpt_indices(self, target, val_dict):
         """
         Get the index of the CPT which corresponds
@@ -98,22 +113,18 @@ class BayesianNet:
         for rv, val in val_dict.items():
             val_idx = self.nodes[rv].value_idx(val)
             rv_idx = []
-            s_idx = val_idx*stride[rv]
+            try:
+                s_idx = val_idx*stride[rv]
+            except KeyError:
+                print("Key Error")
             while s_idx < len(target.cpt):
                 rv_idx.extend(range(s_idx,(s_idx+stride[rv])))
                 s_idx += stride[rv]*card[rv]
             idx = idx.intersection(set(rv_idx))
-        if len(list(idx))>1:
-            print("Len > 1")
+        if len(list(idx)) !=1:
+            print("Len != 1")
         return list(idx)[0]
-    def stride(self, rv, n):
-        if n==rv.name:
-            return 1
-        else:
-            card_list = [rv.card()]
-            card_list.extend([self.nodes[p].card() for p in rv.parents])
-            n_idx = rv.parents.index(n) + 1
-            return int(np.prod(card_list[0:n_idx]))
+
     def original_cpt_indices(self, target, val_dict):
         """
         Get the index of the CPT which corresponds
@@ -138,21 +149,20 @@ class BayesianNet:
         card = dict([(n, self.nodes[n].card()) for n in target.scope()])
         idx = set(range(len(target.originalcpt)))
         for rv, val in val_dict.items():
-            if rv in target.scope():
-                val_idx = self.nodes[rv].value_idx(val)
-                rv_idx = []
-                s_idx = val_idx * stride[rv]
-                while s_idx < len(target.originalcpt):
-                    rv_idx.extend(range(s_idx, (s_idx + stride[rv])))
-                    s_idx += stride[rv] * self.nodes[rv].card()
-                idx = idx.intersection(set(rv_idx))
+            val_idx = self.nodes[rv].value_idx(val)
+            rv_idx = []
+            s_idx = val_idx * stride[rv]
+            while s_idx < len(target.originalcpt):
+                rv_idx.extend(range(s_idx, (s_idx + stride[rv])))
+                s_idx += stride[rv] * card[rv]
+            idx = idx.intersection(set(rv_idx))
         if len(idx) != 1:
             print(target.name," ",val_dict)
         return list(idx)[0]
 
     def plot(self):
         G = nx.Graph()
-        G.add_nodes_from(self.getNodes())
+        G.add_nodes_from(self.getNodeKeys())
         for k, v in self.nodes.items():
             for vv in v.getChildren():
                 G.add_edge(k, vv)
@@ -167,11 +177,11 @@ if __name__ == "__main__":
     bn = BayesianNet()
     bn.readNetwork("resources/cancer3.bn")
     for v in bn.getNodes():
-        print(v,bn.nodes[v].scope())
+        print(v,v.scope())
         d = []
-        for s in bn.nodes[v].scope():
+        for s in v.scope():
             d.append(bn.nodes[s].domain)
-        comb = list(itertools.product(*d))
+        #comb = list(itertools.product(*d))
         #print(v,comb)
         csvFile = 'resources/datasets/10Cases.csv'
         with open(csvFile) as csvfile:

Tester.py
@@ -2,149 +2,180 @@ from BayesianNetwork import BayesianNet
 import numpy as np
 #from MaximumLikelihood import MLE, arrayMLE,static_MLE
 from KLDivergenceCalculation import calculateKLDivergency, meanKL,combinations
-from BayesianEstimate import bayes_estimator
+from BayesianEstimate import bayes_estimator,arrayBE
 bn = BayesianNet()
-bn.readNetwork("resources/cancer3.bn")
+bn.readNetwork("resources/cancer3uniform.bn")
 #comb = combinations(bn)
 #x = bn.cpt_indices(bn.nodes["B"],dict({'A': 'Present', 'B':'Not increased','C':'Present','D':'Absent','E':'Present'}))
 
 #Calculate KL divergency for more datasets of lenght 10 learning parameter from ech trhough Maximum Likelihood
-#array10 = np.array([[BayesianNet, '10Cases.csv']])#, [BayesianNet, '10_1Cases.csv'], [BayesianNet, '10_2Cases.csv'],
-                        #[BayesianNet, '10_3Cases.csv'], [BayesianNet, '10_4Cases.csv'], [BayesianNet, '10_5Cases.csv'],
-                        #[BayesianNet, '10_6Cases.csv'], [BayesianNet, '10_7Cases.csv'], [BayesianNet, '10_8Cases.csv'],
-                        #[BayesianNet, '10_9Cases.csv'], [BayesianNet, '10_10Cases.csv']])
-
-bayes_estimator(bn, 'resources/datasets/10000Cases.csv')
-bn.plot()
-#arrayBE(array10)
-# arrayMLE(array10)
-# #arrayBE(array10)
+# array10 = np.array([BayesianNet, 'resources/datasets/10Cases.csv'], [BayesianNet, 'resources/datasets/10_1Cases.csv'], [BayesianNet, 'resources/datasets/10_2Cases.csv'],
+#                         [BayesianNet, 'resources/datasets/10_3Cases.csv'], [BayesianNet, 'resources/datasets/10_4Cases.csv'], [BayesianNet, 'resources/datasets/10_5Cases.csv'],
+#                         [BayesianNet, 'resources/datasets/10_6Cases.csv'], [BayesianNet, 'resources/datasets/10_7Cases.csv'], [BayesianNet, 'resources/datasets/10_8Cases.csv'],
+#                         [BayesianNet, 'resources/datasets/10_9Cases.csv'], [BayesianNet, 'resources/datasets/10_10Cases.csv'])
 #
-# #Calculate KL divergency for more datasets of lenght 50 learning parameter from ech trhough Maximum Likelihood
+# #bayes_estimator(bn, 'resources/datasets/10000Cases.csv')
+# #bn.plot()
+# arrayBE(array10)
+# # arrayMLE(array10)
+# # #arrayBE(array10)
+# #
+# # #Calculate KL divergency for more datasets of lenght 50 learning parameter from ech trhough Maximum Likelihood
 # array50 = np.array([[BayesianNet, '50Cases.csv'], [BayesianNet, '50_1Cases.csv'], [BayesianNet, '50_2Cases.csv'],
 #                         [BayesianNet, '50_3Cases.csv'], [BayesianNet, '50_4Cases.csv'], [BayesianNet, '50_5Cases.csv'],
 #                         [BayesianNet, '50_6Cases.csv'], [BayesianNet, '50_7Cases.csv'], [BayesianNet, '50_8Cases.csv'],
 #                         [BayesianNet, '50_9Cases.csv'], [BayesianNet, '50_10Cases.csv']])
 #
-# arrayMLE(array50)
-# #arrayBE(array50)
+# #arrayMLE(array50)
+# arrayBE(array50)
 #
 # #Calculate KL divergency for more datasets of lenght 100 learning parameter from ech trhough Maximum Likelihood
-# array100 = np.array([[BayesianNet, '100Cases.csv'], [BayesianNet, '100_1Cases.csv'], [BayesianNet, '100_2Cases.csv'],
-#                         [BayesianNet, '100_3Cases.csv'], [BayesianNet, '100_4Cases.csv'], [BayesianNet, '100_5Cases.csv'],
-#                         [BayesianNet, '100_6Cases.csv'], [BayesianNet, '100_7Cases.csv'], [BayesianNet, '100_8Cases.csv'],
-#                         [BayesianNet, '100_9Cases.csv'], [BayesianNet, '100_10Cases.csv']])
+# array100 = np.array([[BayesianNet, 'resources/datasets/100Cases.csv'], [BayesianNet, 'resources/datasets/100_1Cases.csv'], [BayesianNet, 'resources/datasets/100_2Cases.csv'],
+#                         [BayesianNet, 'resources/datasets/100_3Cases.csv'], [BayesianNet, 'resources/datasets/100_4Cases.csv'], [BayesianNet, 'resources/datasets/100_5Cases.csv'],
+#                         [BayesianNet, 'resources/datasets/100_6Cases.csv'], [BayesianNet, 'resources/datasets/100_7Cases.csv'], [BayesianNet, 'resources/datasets/100_8Cases.csv'],
+#                         [BayesianNet, 'resources/datasets/100_9Cases.csv'], [BayesianNet, 'resources/datasets/100_10Cases.csv']])
 #
-# arrayMLE(array100)
-# #arrayBE(array100)
+# #arrayMLE(array100)
+# arrayBE(array100)
+bNet10 = BayesianNet()
+bNet10.readNetwork("resources/cancer3uniform.bn")
+csvfile10 = 'resources/datasets/10Cases.csv'
+bayes_estimator(csvfile10, bNet10)
+print('KL divergence for n = 10', calculateKLDivergency(bNet10))
 #
-# #Calculate MLE for all other datasets
+#Calculate MLE for all other datasets
 # bNet250 = BayesianNet()
-# bNet250.readNetwork("resources/cancer3.bn")
-# csvfile250 = '250Cases.csv'
-# BE(csvfile250, bNet250)
-# #
+# bNet250.readNetwork("resources/cancer3uniform.bn")
+csvfile250 = 'resources/datasets/250Cases.csv'
+#bayes_estimator(csvfile250, bNet250)
+bayes_estimator(csvfile250, bNet10)
+print('KL divergence for n = 250', calculateKLDivergency(bNet10))
+#
 # bNet500 = BayesianNet()
-# bNet500.readNetwork("resources/cancer3.bn")
-# csvfile500 = '500Cases.csv'
-# BE(csvfile500, bNet500)
-#
+# bNet500.readNetwork("resources/cancer3uniform.bn")
+csvfile500 = 'resources/datasets/500Cases.csv'
+#bayes_estimator(csvfile500, bNet500)
+bayes_estimator(csvfile500, bNet10)
+print('KL divergence for n = 500', calculateKLDivergency(bNet10))
 # bNet750 = BayesianNet()
-# bNet750.readNetwork("resources/cancer3.bn")
-# csvfile750 = '750Cases.csv'
-# BE(csvfile750, bNet750)
-#
+# bNet750.readNetwork("resources/cancer3uniform.bn")
+csvfile750 = 'resources/datasets/750Cases.csv'
+#bayes_estimator(csvfile750, bNet750)
+bayes_estimator(csvfile750, bNet10)
+print('KL divergence for n = 750', calculateKLDivergency(bNet10))
+
 # bNet1000 = BayesianNet()
-# bNet1000.readNetwork("resources/cancer3.bn")
-# csvfile1000 = '1000Cases.csv'
-# BE(csvfile1000, bNet1000)
-#
+# bNet1000.readNetwork("resources/cancer3uniform.bn")
+csvfile1000 = 'resources/datasets/1000Cases.csv'
+#bayes_estimator(csvfile1000, bNet1000)
+bayes_estimator(csvfile1000, bNet10)
+print('KL divergence for n = 1000', calculateKLDivergency(bNet10))
 # bNet1250 = BayesianNet()
-# bNet1250.readNetwork("resources/cancer3.bn")
-# csvfile1250 = '1250Cases.csv'
-# BE(csvfile1250, bNet1250)
-#
+# bNet1250.readNetwork("resources/cancer3uniform.bn")
+csvfile1250 = 'resources/datasets/1250Cases.csv'
+#bayes_estimator(csvfile1250, bNet1250)
+bayes_estimator(csvfile1250, bNet10)
+print('KL divergence for n = 1250', calculateKLDivergency(bNet10))
 # bNet1500 = BayesianNet()
-# bNet1500.readNetwork("resources/cancer3.bn")
-# csvfile1500 = '1500Cases.csv'
-# BE(csvfile1500, bNet1500)
-#
+# bNet1500.readNetwork("resources/cancer3uniform.bn")
+csvfile1500 = 'resources/datasets/1500Cases.csv'
+#bayes_estimator(csvfile1500, bNet1500)
+bayes_estimator(csvfile1500, bNet10)
+print('KL divergence for n = 1500', calculateKLDivergency(bNet10))
 # bNet1750 = BayesianNet()
-# bNet1750.readNetwork("resources/cancer3.bn")
-# csvfile1750 = '1750Cases.csv'
-# BE(csvfile1750, bNet1750)
-#
-# bNet2000 = BayesianNet()
-# bNet2000.readNetwork("resources/cancer3.bn")
-# csvfile2000 = '2000Cases.csv'
-# BE(csvfile2000, bNet2000)
-#
+# bNet1750.readNetwork("resources/cancer3uniform.bn")
+csvfile1750 = 'resources/datasets/1750Cases.csv'
+#bayes_estimator(csvfile1750, bNet1750)
+bayes_estimator(csvfile1750, bNet10)
+print('KL divergence for n = 1750', calculateKLDivergency(bNet10))
+#bNet2000 = BayesianNet()
+#bNet2000.readNetwork("resources/cancer3uniform.bn")
+csvfile2000 = 'resources/datasets/2000Cases.csv'
+#bayes_estimator(csvfile2000, bNet2000)
+bayes_estimator(csvfile2000, bNet10)
+print('KL divergence for n = 2000', calculateKLDivergency(bNet10))
 # bNet2250 = BayesianNet()
-# bNet2250.readNetwork("resources/cancer3.bn")
-# csvfile2250 = '2250Cases.csv'
-# BE(csvfile2250, bNet2250)
-#
+# bNet2250.readNetwork("resources/cancer3uniform.bn")
+csvfile2250 = 'resources/datasets/2250Cases.csv'
+# bayes_estimator(csvfile2250, bNet2250)
+bayes_estimator(csvfile2250, bNet10)
+print('KL divergence for n = 2250', calculateKLDivergency(bNet10))
 # bNet2500 = BayesianNet()
-# bNet2500.readNetwork("resources/cancer3.bn")
-# csvfile2500 = '2500Cases.csv'
-# BE(csvfile2500, bNet2500)
-#
+# bNet2500.readNetwork("resources/cancer3uniform.bn")
+csvfile2500 = 'resources/datasets/2500Cases.csv'
+#bayes_estimator(csvfile2500, bNet2500)
+bayes_estimator(csvfile2500, bNet10)
+print('KL divergence for n = 2500', calculateKLDivergency(bNet10))
 # bNet3000 = BayesianNet()
-# bNet3000.readNetwork("resources/cancer3.bn")
-# csvfile3000 = '3000Cases.csv'
-# BE(csvfile3000, bNet3000)
-#
-# # bNet3500 = BayesianNet()
-# # bNet3500.readNetwork("resources/cancer3.bn")
-# # csvfile3500 = '3500Cases.csv'
-# # BE(csvfile3500, bNet3500)
-#
+# bNet3000.readNetwork("resources/cancer3uniform.bn")
+csvfile3000 = 'resources/datasets/3000Cases.csv'
+#bayes_estimator(csvfile3000, bNet3000)
+bayes_estimator(csvfile3000, bNet10)
+print('KL divergence for n = 3000', calculateKLDivergency(bNet10))
+# bNet3500 = BayesianNet()
+# bNet3500.readNetwork("resources/cancer3uniform.bn")
+# csvfile3500 = '3500Cases.csv'
+# bayes_estimator(csvfile3500, bNet3500)
+
 # bNet4000 = BayesianNet()
-# bNet4000.readNetwork("resources/cancer3.bn")
-# csvfile4000 = '4000Cases.csv'
-# BE(csvfile4000, bNet4000)
-#
-# bNet4500 = BayesianNet()
-# bNet4500.readNetwork("resources/cancer3.bn")
-# csvfile4500 = '4500Cases.csv'
-# BE(csvfile4500, bNet4500)
-#
-# bNet5000 = BayesianNet()
-# bNet5000.readNetwork("resources/cancer3.bn")
-# csvfile5000 = '5000Cases.csv'
-# BE(csvfile5000, bNet5000)
-#
+# bNet4000.readNetwork("resources/cancer3uniform.bn")
+#csvfile4000 = 'resources/datasets/4000Cases.csv'
+#bayes_estimator(csvfile4000, bNet4000)
+
+#bNet4000 = BayesianNet()
+#bNet4000.readNetwork("resources/cancer3uniform.bn")
+csvfile4000 = 'resources/datasets/4000Cases.csv'
+bayes_estimator(csvfile4000, bNet10)
+print('KL divergence for n = 4000', calculateKLDivergency(bNet10))
+#bNet4500 = BayesianNet()
+#bNet4500.readNetwork("resources/cancer3uniform.bn")
+csvfile4500 = 'resources/datasets/4500Cases.csv'
+bayes_estimator(csvfile4500, bNet10)
+print('KL divergence for n = 4500', calculateKLDivergency(bNet10))
+#bNet5000 = BayesianNet()
+#bNet5000.readNetwork("resources/cancer3uniform.bn")
+csvfile5000 = 'resources/datasets/5000Cases.csv'
+#bayes_estimator(csvfile5000, bNet5000)
+bayes_estimator(csvfile5000, bNet10)
+print('KL divergence for n = 5000', calculateKLDivergency(bNet10))
 # bNet6000 = BayesianNet()
-# bNet6000.readNetwork("resources/cancer3.bn")
-# csvfile6000 = '6000Cases.csv'
-# BE(csvfile6000, bNet6000)
-#
+# bNet6000.readNetwork("resources/cancer3uniform.bn")
+csvfile6000 = 'resources/datasets/6000Cases.csv'
+#bayes_estimator(csvfile6000, bNet6000)
+bayes_estimator(csvfile6000, bNet10)
+print('KL divergence for n = 6000', calculateKLDivergency(bNet10))
 # bNet7000 = BayesianNet()
-# bNet7000.readNetwork("resources/cancer3.bn")
-# csvfile7000 = '7000Cases.csv'
-# BE(csvfile7000, bNet7000)
-#
+# bNet7000.readNetwork("resources/cancer3uniform.bn")
+csvfile7000 = 'resources/datasets/7000Cases.csv'
+#bayes_estimator(csvfile7000, bNet7000)
+bayes_estimator(csvfile7000, bNet10)
+print('KL divergence for n = 7000', calculateKLDivergency(bNet10))
 # bNet8000 = BayesianNet()
-# bNet8000.readNetwork("resources/cancer3.bn")
-# csvfile8000 = '8000Cases.csv'
-# BE(csvfile8000, bNet8000)
-#
+# bNet8000.readNetwork("resources/cancer3uniform.bn")
+csvfile8000 = 'resources/datasets/8000Cases.csv'
+#bayes_estimator(csvfile8000, bNet8000)
+bayes_estimator(csvfile8000, bNet10)
+print('KL divergence for n = 8000', calculateKLDivergency(bNet10))
+
 # bNet9000 = BayesianNet()
-# bNet9000.readNetwork("resources/cancer3.bn")
-# csvfile9000 = '9000Cases.csv'
-# BE(csvfile9000, bNet9000)
-#
+# bNet9000.readNetwork("resources/cancer3uniform.bn")
+csvfile9000 = 'resources/datasets/9000Cases.csv'
+#bayes_estimator(csvfile9000, bNet9000)
+bayes_estimator(csvfile9000, bNet10)
+print('KL divergence for n = 9000', calculateKLDivergency(bNet10))
+
 # bNet10000 = BayesianNet()
-# bNet10000.readNetwork("resources/cancer3.bn")
-# csvfile10000 = '10000Cases.csv'
-# BE(csvfile10000, bNet10000)
-#
-#
-# #print All Kullback-Leibler for all learned network
+# bNet10000.readNetwork("resources/cancer3uniform.bn")
+csvfile10000 = 'resources/datasets/10000Cases.csv'
+#bayes_estimator(csvfile10000, bNet10000)
+bayes_estimator(csvfile10000, bNet10)
+print('KL divergence for n = 10000', calculateKLDivergency(bNet10))
+#print All Kullback-Leibler for all learned network
 # print('KL divergence for n = 10',meanKL(array10))
 # print('KL divergence for n = 50', meanKL(array50))
 # print('KL divergence for n = 100',meanKL(array100))
-# print('KL divergence for n = 250', calculateKLDivergency(bNet250))
+
+#print('KL divergence for n = 10', calculateKLDivergency(bNet10))
 # print('KL divergence for n = 500', calculateKLDivergency(bNet500))
 # print('KL divergence for n = 750', calculateKLDivergency(bNet750))
 # print('KL divergence for n = 1000', calculateKLDivergency(bNet1000))
@@ -164,6 +195,6 @@ bn.plot()
 # print('KL divergence for n = 8000', calculateKLDivergency(bNet8000))
 # print('KL divergence for n = 9000', calculateKLDivergency(bNet9000))
 # print('KL divergence for n = 10000', calculateKLDivergency(bNet10000))
-
+#
 
 